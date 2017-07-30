625e09e1e382a9cdf2d595b3a8c3d4f73b068f0d
tanzahmed <ahmedtufailtanzeem@gmail.com>
2017-07-30 17:14:26 +0530

    Initial commit framework with logger and Jenkins integration

---
+++ src/test/java/org/tanzeem/test/Tester.java
@@ -0,0 +1,19 @@
+package org.tanzeem.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tester {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Tester.class);
+
+	@Test
+	public void assertTrue() {
+
+		LOG.info("Running sample test");
+		Assert.assertTrue(true, "Value mismatch");
+	}
+
+}
